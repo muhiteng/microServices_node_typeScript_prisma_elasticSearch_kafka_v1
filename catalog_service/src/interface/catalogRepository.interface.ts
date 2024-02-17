2333939eb131558e3037c613b6a23b227132e1a7
muh.it.eng.19841984@gmail.com <muh.it.eng.19841984@gmail.com>
2024-02-17 11:17:55 -0800

    mean architecture for catalog service

---
+++ catalog_service/src/interface/catalogRepository.interface.ts
@@ -0,0 +1,9 @@
+import { Product } from "../models/product.model";
+
+export interface ICatalogRepository {
+  create(data: Product): Promise<Product>;
+  update(data: Product): Promise<Product>;
+  delete(id: any):void;
+  find(limit: number, offset: number): Promise<Product[]>;
+  findOne(id: number): Promise<Product>;
+}
---
+++ catalog_service/src/repository/catalog.repository.ts
@@ -0,0 +1,21 @@
+import { ICatalogRepository } from "../interface/catalogRepository.interface";
+import { Product } from "../models/product.model";
+
+
+export class CatalogRepository implements ICatalogRepository {
+    create(data: Product): Promise<Product> {
+        throw new Error("Method not implemented.");
+    }
+    update(data: Product): Promise<Product> {
+        throw new Error("Method not implemented.");
+    }
+    delete(id: any) {
+        throw new Error("Method not implemented.");
+    }
+    find(limit: number, offset: number): Promise<Product[]> {
+        throw new Error("Method not implemented.");
+    }
+    findOne(id: number): Promise<Product> {
+        throw new Error("Method not implemented.");
+    }
+}
